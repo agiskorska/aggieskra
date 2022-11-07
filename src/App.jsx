edbf4148d490ecf54455b0ed259fedf52801a314
agiskorska <skorskaga@gmail.com>
2022-11-07 19:44:49 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {Routes, Route} from 'react-router-dom'
+
+import './App.css'
+import {Home} from './Pages'
+function App() {
+
+  return (
+    <div className="App">
+      <Home ></Home>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,9 @@
+import styles from './index.module.css'
+
+function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
