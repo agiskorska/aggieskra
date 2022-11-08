fde395139acb20158184ff79113bc0b1c0f97cce
agiskorska <skorskaga@gmail.com>
2022-11-08 13:29:32 +0000

    Header links

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -5,10 +5,6 @@ import {ArticleSection, Button, Container, Heading} from '../../common'
 function Home() {
   return (
     <div>
-      <Container>
-        <Heading>Welcome to my website</Heading>
-        <ArticleSection>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero nam quam omnis. Tempora doloremque consectetur iusto odit fugiat, facilis labore autem? Commodi distinctio quidem repudiandae molestias, quaerat quos voluptate officia mollitia cum laudantium reprehenderit deserunt dolore aperiam autem aspernatur. Excepturi provident dolores debitis natus tempore non, at error? In, totam.</ArticleSection>
-      </Container>
     </div>
   )
 }
--- src/common/Header/index.jsx
+++ src/common/Header/index.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import { Button, NavBar } from '../'
 import styles from './index.module.css'
 import sample from '../../assets/sample.jpg'
 
 function Header() {
-  
+
   return (
     <header className={styles.root}>
         <NavBar />
+        <div className={styles.push}></div>
+        <nav className={styles.nav}>
+          <NavLink className={styles.navlinkLeft} to='/'><span>Home</span></NavLink>
+          <NavLink className={styles.navlinkRight} to='/about'><span>About</span></NavLink>
+          <NavLink className={styles.navlinkLeft} to='/offer'><span>My offer</span></NavLink>
+          <NavLink className={styles.navlinkRight} to='/contact'><span>Contact Me</span></NavLink>
+        </nav>
+
         <section className={styles.hero}>
           <img src={sample} alt='sample image' className={styles.heroImg}/>
         </section>
