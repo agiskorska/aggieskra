2da9d3546bb961afa4df4de88faf508643bf7b1d
agiskorska <skorskaga@gmail.com>
2022-11-08 13:55:29 +0000

    Jumpy transition, add padding to name

--- src/common/NavBar/index.jsx
+++ src/common/NavBar/index.jsx
@@ -5,17 +5,23 @@ import styles from './index.module.css'
 
 function Navbar() {
   const [height, setHeight] = useState('50vh')
+  const [direction, setDirection] = useState('column')
+  const [justify, setJustify] = useState('flex-end')
   useEffect(() => {
-    const timer = setTimeout(()=> {
-        console.log('changing height')
+    const timer1 = setTimeout(()=> {
         setHeight('10vh')
     }, 1500)
+    const timer = setTimeout(()=> {
+      setDirection('row')
+      setJustify('flex-start')
+  }, 2700)
     
   }, [])
 
+
   return (
     <div className={styles.root} style={{height:height, transition: 'all 1.5s'}}>
-        <div className={styles.logos}>
+        <div className={styles.logos} style={{ flexDirection:direction, justifyContent: justify,transition: 'all 1.5s'}}>
             <img src={logo}  alt="logo" className={styles.logo}/>
             <div className={styles.title}>Aggie Skorska</div>
         </div>
