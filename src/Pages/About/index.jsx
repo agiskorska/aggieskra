bc61689ce8cf480ac3e522cc8866c6459aaee6bb
agiskorska <skorskaga@gmail.com>
2022-11-08 22:44:33 +0000

    Update about

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -5,13 +5,12 @@ import {ArticleSection, Button, Container, Heading} from '../../common'
 function About() {
   return (
     <Container>
-          <ArticleSection>
-      <Heading>About my work</Heading>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae temporibus ipsam iste eligendi tempore? Unde autem impedit modi voluptatibus doloribus aspernatur doloremque repudiandae beatae quae commodi ipsam ea dolor quas reiciendis, sequi animi veniam. Incidunt maiores reprehenderit, optio animi fugit molestiae modi! Earum ratione eius minima veritatis nihil placeat animi!</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae temporibus ipsam iste eligendi tempore? Unde autem impedit modi voluptatibus doloribus aspernatur doloremque repudiandae beatae quae commodi ipsam ea dolor quas reiciendis, sequi animi veniam. Incidunt maiores reprehenderit, optio animi fugit molestiae modi! Earum ratione eius minima veritatis nihil placeat animi!</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae temporibus ipsam iste eligendi tempore? Unde autem impedit modi voluptatibus doloribus aspernatur doloremque repudiandae beatae quae commodi ipsam ea dolor quas reiciendis, sequi animi veniam. Incidunt maiores reprehenderit, optio animi fugit molestiae modi! Earum ratione eius minima veritatis nihil placeat animi!</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae temporibus ipsam iste eligendi tempore? Unde autem impedit modi voluptatibus doloribus aspernatur doloremque repudiandae beatae quae commodi ipsam ea dolor quas reiciendis, sequi animi veniam. Incidunt maiores reprehenderit, optio animi fugit molestiae modi! Earum ratione eius minima veritatis nihil placeat animi!</p>
-    </ArticleSection>
+      <ArticleSection>
+        <Heading>About me</Heading>
+        <p>Passionate about development, self-driven, ambitious developer collaborating with a team of like minded people with aspiration to make the World Wide Web more accessible to all.</p>
+        <Heading>My work</Heading>
+        <p></p>
+      </ArticleSection>
     </Container>
   )
 }
