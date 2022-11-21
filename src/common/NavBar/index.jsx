169b3be366322eafa53618f2dddc10ff894ae056
agiskorska <skorskaga@gmail.com>
2022-11-21 22:44:23 +0000

    Fix logo transition

--- src/common/NavBar/index.jsx
+++ src/common/NavBar/index.jsx
@@ -5,25 +5,28 @@ import styles from './index.module.css'
 
 function Navbar() {
   const [height, setHeight] = useState('50vh')
-  const [direction, setDirection] = useState('column')
+  const [opacity, setOpacity] = useState(1)
   const [justify, setJustify] = useState('flex-end')
   useEffect(() => {
     const timer1 = setTimeout(()=> {
         setHeight('10vh')
     }, 2000) // add extra 500ms delay
     const timer = setTimeout(()=> {
-      setDirection('row')
-      setJustify('flex-start')
-  }, 3200) // add extra 500ms delay
+      setOpacity(0)
+  }, 2500) // add extra 500ms delay
     
   }, [])
 
 
   return (
     <div className={styles.root} style={{height:height, transition: 'all 1.5s'}}>
-        <div className={styles.logos} style={{ flexDirection:direction, justifyContent: justify,transition: 'all 1.5s'}}>
+        <div className={styles.logos} style={{ opacity:opacity, justifyContent: justify,transition: 'all 1.5s'}}>
             <img src={logo}  alt="logo" className={styles.logo}/>
             <div className={styles.title}>Aggie Skorska</div>
+        </div>
+        <div className={styles.logosLeft} style={{ opacity:opacity ? '0' : '1', justifyContent: justify,transition: 'all 1.5s'}}>
+            <img src={logo}  alt="logo" className={styles.logoLeft}/>
+            <div className={styles.titleLeft}>Aggie Skorska</div>
         </div>
     </div>
   )
