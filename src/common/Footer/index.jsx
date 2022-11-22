ae0e00d6771f362f08c30145b08f805a7a9aa33b
agiskorska <skorskaga@gmail.com>
2022-11-22 14:10:33 +0000

    More rwd

--- src/common/Footer/index.jsx
+++ src/common/Footer/index.jsx
@@ -2,15 +2,20 @@ import {useEffect, useState} from 'react'
 import styles from './index.module.css'
 function Footer() {
   const [height, setHeight] = useState('56vh')
+  const [opacity, setOpacity] = useState(0)
   useEffect(() => {
     const timer = setTimeout(()=> {
         setHeight('150px')
+        setTimeout(()=> {setOpacity(1)}, 1500)
     }, 2000) // add extra 500ms delay
     
   }, [])
   return (
     <footer className={styles.root} style={{height: height, transition: 'all 1.5s'}}>
       <p className={styles.p}>Website Design & Development</p>
+      <div className={styles.data} style={{opacity: opacity, transition: 'all 1.5s'}}>
+        <p>+44 7598 397 503 </p> <p>contact@aggieskorska.co.uk</p>
+      </div>
     </footer>
   )
 }
