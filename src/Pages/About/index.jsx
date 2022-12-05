6573dafcc6da5b7534529f235b925a9f4dca1315
agiskorska <skorskaga@gmail.com>
2022-12-05 15:36:06 +0000

    More on About

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -8,6 +8,7 @@ function About() {
       <ArticleSection>
         <Heading>About me</Heading>
         <p>Passionate about development, self-driven, ambitious developer collaborating with a team of like minded people with aspiration to make the World Wide Web more accessible to all.</p>
+        <p>I create my websites in technology called ReactJS. It is a special framework in JavaScript that allows for very quick re-rendering of a website and by doing so it is quick, seamless and efficient. Instead of reloading a website after moving to another tab, React repaints the current website with new content. That's why it is so efficient and neat leaving such technologies as WordPress or Wix behind.</p>
         <Heading>What you can expect</Heading>
         <p>I have strong skills in web development and I am constantly improving my knowledge about design and User Experience (UX). I know how important the fist impression can be and how much a business relies on it, so I will make sure your website is unique, easy to use and memorable. These features can give you an advantage over your competition.</p>
         <p>These days many businesses have outdated websites. Your client opens your website (most likely on their phone) and if they find it difficult to use, they're going to go back to their Google search and get the next link that would most likely be your competition. </p>
